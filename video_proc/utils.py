652a495a963acc5cbffcd1a7cf1f569840c0d0a9
Ayatafoy <romanow.lesha2013@yandex.ru>
2021-02-02 20:31:42 +0400

    fix visualization

--- video_proc/utils.py
+++ video_proc/utils.py
@@ -31,15 +31,49 @@ def put_text(img: np.ndarray,
     """
     x, y, w, h = bbox
 
-    img = cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+    pivot_img_size = 112
+    resolution_x = img.shape[1];
 
-    img = cv2.putText(img, person.age, (int(x), int(y + h)), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+    overlay = img.copy()
+    opacity = 0.4
 
-    img = cv2.putText(img, person.gender, (int(x), int(y + h+22)), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+    img = cv2.rectangle(img, (x, y), (x + w, y + h), (67, 67, 67), 1)
+    if x + w + pivot_img_size < resolution_x:
+        # right
+        cv2.rectangle(img, (x + w, y), (x + w + pivot_img_size + 20, y + h), (64, 64, 64), cv2.FILLED)
+        cv2.addWeighted(overlay, opacity, img, 1 - opacity, 0, img)
 
-    img = cv2.putText(img, person.race, (int(x), int(y + h + 44)), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+    elif x - pivot_img_size > 0:
+        # left
+        cv2.rectangle(img, (x - pivot_img_size - 20, y), (x, y + h), (64, 64, 64), cv2.FILLED)
+        cv2.addWeighted(overlay, opacity, img, 1 - opacity, 0, img)
 
-    img = cv2.putText(img, person.percent_in_video(all_frames_cnt), (int(x), int(y + h + 66)),
-                      cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+    gender_label = person.gender
+    race_label = person.race
+    age_label = person.age
+    num_frames = person.percent_in_video(all_frames_cnt)
+
+    attributes = [gender_label, race_label, age_label, num_frames]
+
+    for index, attribute in enumerate(attributes):
+        bar_x = 35  # this is the size if an emotion is 100%
+
+        if x + w + pivot_img_size < resolution_x:
+
+            text_location_y = y + 20 + (index + 1) * 20
+            text_location_x = x + w
+
+            if text_location_y < y + h:
+                cv2.putText(img, attribute, (text_location_x + 10, text_location_y), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                            (255, 255, 255), 1)
+
+        elif x - pivot_img_size > 0:
+
+            text_location_y = y + 20 + (index + 1) * 20
+            text_location_x = x - pivot_img_size
+
+            if text_location_y <= y + h:
+                cv2.putText(img, attribute, (text_location_x + 10, text_location_y), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                            (255, 255, 255), 1)
 
     return img
